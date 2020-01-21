43be8df35c9dd8cc41c5a6fc72060ff6ee88e141
eddysims <eddy.s@getjobber.com>
2020-01-21 14:24:23 -0700

    Lesson 1

---
+++ ruby_projects/hello_world.rb
@@ -0,0 +1,17 @@
+# Say hello world in multiple ways
+
+# Simple put string
+# Using a variable
+# Using a method
+
+puts "Hello World" # Returns nil
+p "Hello World" # Returns "Hello World"
+
+greeting = "Hello World"
+puts greeting
+
+def say_something(thing_to_say)
+    puts thing_to_say
+end
+
+say_something "Hello World"
